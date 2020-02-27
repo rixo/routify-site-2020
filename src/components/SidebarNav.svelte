f9cb8b4c576495a6a901638602e9dcd92212c055
Johan Ronsse <johan.ronsse@gmail.com>
2020-02-27 22:29:51 +0100

    Try to make a svelte component out of the side navigation

---
+++ src/components/SidebarNav.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { isActive, url } from '@sveltech/routify'
+
+    export let items = [];
+
+    $: items = items.map(([url, name]) => {
+      return {
+        name,
+        url: $url(url),
+        active: $isActive(url)
+      }
+    });
+    
+</script>
+
+<ul class="c-sidebar-nav">
+    {#each items as {name, url, active}}
+        <li class:c-sidebar-nav__item--selected={active} class="c-sidebar-nav__item">
+            <a href="{url}">{name}</a>
+        </li>
+    {/each}
+</ul>
+
---
+++ src/pages/configuration/_layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import SidebarNav from '../../components/SidebarNav.svelte';
+
+    let items =
+        [
+            ['/configuration/basics', 'Basics'],
+            ['/configuration/code-splitting', 'Code-splitting'],
+            ['/configuration/prerendering', 'Prerendering'],
+            ['/configuration/deployments', 'Deployments'],
+            ['/configuration/ssr', 'SSR'],
+            ['/configuration/cache', 'Cache']
+        ]
+
+</script>
+
+<main class="c-sidebar-layout">
+    <div class="c-sidebar-layout__side">
+        <nav>
+            <SidebarNav items />
+        </nav>
+    </div>
+    <div class="c-sidebar-layout__main">
+        <slot />
+    </div>
+</main>
