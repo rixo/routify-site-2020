0eec89724356a83aa12c908599905a28b5534667
Johan Ronsse <johan.ronsse@gmail.com>
2020-03-24 17:29:06 +0100

    Dropdown

---
+++ src/pages/dropdowndemo.svelte
@@ -0,0 +1,11 @@
+<!-- routify:options $index=false -->
+<nav role="navigation" class="c-navigation">
+    <ul>
+        <li><a href="/guide">guide</a> </li>
+        <li><a href="/docs">docs</a> </li>
+        <li><a href="/examples">examples</a> </li>
+        <li>
+            <a href="https://github.com/sveltech/routify" class="c-navigation__github-link"><img class="c-navigation__github-logo" src="/images/github.svg" alt="Github logo"></a>
+        </li>
+    </ul>
+</nav>
