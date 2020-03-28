1301466c02409e2d85fc7938d2698824ce4d8382
Jakob Rosenberg <jakobrosenberg@gmail.com>
2020-03-28 20:10:51 +0100

    subtle transition

--- src/components/Transitions.svelte
+++ src/components/Transitions.svelte
@@ -1,38 +1,15 @@
 <script>
-  import { BaseTransition } from "@sveltech/routify/decorators";
-  import { getConcestor } from '@sveltech/routify'
-  import { scale, fly } from "svelte/transition";
+  import { BaseTransition } from "@sveltech/routify/decorators"
+  import { fade } from "svelte/transition";
+  const duration = 100;
   const configs = [
     {
-      condition: meta => _getDirection(meta) > 0,
-      transition: fly,
-      inParams: { y: 400 },
-      outParams: { y: -400 }
-    },
-    {
-      condition: meta => _getDirection(meta) < 0,
-      transition: fly,
-      inParams: { y: -400 },
-      outParams: { y: 400 }
-    },
-    {
       condition: () => true,
-      transition: () => {}
+      transition: fade,
+      inParams: { duration },
+      outParams: { duration }
     }
   ];
-
-  function _getDirection(meta) {
-    const { routes } = meta;
-    const components = routes.map(r =>
-      r.meta.$index === false && r.isIndex ? r.parent : r
-    );
-    const tabs = routes.map(r => r.path.split('/')[1])
-    const [, cmp1, cmp2] = getConcestor(components[0], components[1])
-
-    const direction = cmp1 && cmp2 && cmp1.meta.$index - cmp2.meta.$index
-
-    return tabs[0] === tabs[1] && direction;
-  }
 </script>
 
 <BaseTransition {configs}>
