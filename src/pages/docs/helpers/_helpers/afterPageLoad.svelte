b07feeafd74aec7aeff301c3e360f8a652e06896
Johan Ronsse <johan.ronsse@gmail.com>
2020-05-23 10:43:43 +0200

    afterPageLoad docs WIP

---
+++ src/pages/docs/helpers/_helpers/afterPageLoad.svelte
@@ -0,0 +1,31 @@
+<script>
+  import FunctionDoc from "@/components/FunctionDoc.svelte";
+  import Code from "@/components/Code.svelte";
+
+  const params = [
+    {
+      type: "Function",
+      name: "callback",
+      description: "TBD"
+    }
+  ];
+</script>
+
+<FunctionDoc name="$afterPageLoad" {params}>
+  <p>
+    <code>$afterPageLoad</code>
+    TBD
+  </p>
+
+  <Code language="svelte">
+    {`
+      <`}{`script>
+        import { afterPageLoad } from "@sveltech/routify"
+        $afterPageLoad ((page) => {
+          console.log(page.title);
+        })
+      </script>
+    `}
+  </Code>
+
+</FunctionDoc>
